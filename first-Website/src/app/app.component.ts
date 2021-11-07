404aff98a3de88711b95d0de7c8ae7d6a707ace5
AndrewHooker <andrewhooker2@gmail.com>
2021-11-06 20:45:09 -0500

    first commit for Angular Website

---
+++ first-Website/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+   
+   <app-home></app-home>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'first-Website';
+}
